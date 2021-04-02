c5ddb5fd16cdbcacf37f21efed57dae03bc6572c
Virinas-code <alexcode228@gmail.com>
2021-04-02 15:53:54 +0200

    Crocrodile v1.1.0 installator

--- install-crocrodile.sh
+++ install-crocrodile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-wget https://github.com/Virinas-code/Crocrodile/archive/refs/tags/v1.0.2.tar.gz
-tar -zxvf v1.0.2.tar.gz
-cd Crocrodile-1.0.2
+wget https://github.com/Virinas-code/Crocrodile/archive/refs/tags/v1.1.0.tar.gz
+tar -zxvf v1.1.0.tar.gz
+cd Crocrodile-1.1.0
 read -p "Token : "
 echo "$REPLY" > lichess.token
 clear
