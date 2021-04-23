215f38f6976609bca77c69a13dc75afa172a9c18
Virinas-code <alexcode228@gmail.com>
2021-04-23 15:15:16 +0200

    Linting for my_engine/nn_auto.py

--- my_engine/nn_auto.py
+++ my_engine/nn_auto.py
@@ -9,18 +9,18 @@ import chess  # python-chess
 
 if __name__ == '__main__':
     print("Enter nothing to quit.")
-    inner = "continue"
-    while inner != "":
-        inner = input("FEN: ")
-        if inner:
-            board = chess.Board(inner)
+    INNER = "continue"
+    while INNER != "":
+        INNER = input("FEN: ")
+        if INNER:
+            board = chess.Board(INNER)
             print("Legal moves :", ", ".join(move.uci() for move in board.legal_moves))
             print("Good moves :")
             good_moves = list()
-            good_move_input = "continue"
-            while good_move_input != "":
-                good_move_input = input()
-                good_moves.append(good_move_input)
+            GOOD_MOVE_INPUT = "continue"
+            while GOOD_MOVE_INPUT != "":
+                GOOD_MOVE_INPUT = input()
+                good_moves.append(GOOD_MOVE_INPUT)
             good_moves.pop()
             moves = list()
             for move in board.legal_moves:
@@ -31,11 +31,11 @@ if __name__ == '__main__':
             print("Bad moves :", ", ".join(move.uci() for move in moves))
             print("Generated file extension :")
             print("====================")
-            string = str()
+            STRING = str()
             for move in moves:
-                string += board.fen() + "\n"
-                string += move.uci() + "\n\n"
-            print(string)
+                STRING += board.fen() + "\n"
+                STRING += move.uci() + "\n\n"
+            print(STRING)
             print("====================")
             confirm = input("Autowrite in file ? [Y/n] ")
             if confirm.lower() == "n" or confirm.lower() == "no":
@@ -46,10 +46,10 @@ if __name__ == '__main__':
                 file = open("my_engine/training_mauvaiscoups_ouverture_blancs.txt", 'a')
                 if read[-2:] == "\n\n":
                     pass
-                elif string[-1] == "\n":
+                elif STRING[-1] == "\n":
                     file.write("\n")
-                elif string[-1] != "\n":
+                elif STRING[-1] != "\n":
                     file.write("\n\n")
-                file.write(string[:-2])
+                file.write(STRING[:-2])
                 file.close()
                 print("Done.")
