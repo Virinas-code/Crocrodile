a59c6e48818b607f83e6abbc79dc0db692ba6f3b
Virinas-code <alexcode228@gmail.com>
2021-05-03 08:55:01 +0200

    Add 3 layers (random)

--- my_engine/nn_random.py
+++ my_engine/nn_random.py
@@ -25,9 +25,15 @@ if continuer == "y":
     w1 = numpy.random.rand(74, 74) * 2 - 1
     w2 = numpy.random.rand(74, 74) * 2 - 1
     w3 = numpy.random.rand(74, 74) * 2 - 1
-    w4 = numpy.random.rand(74, 1) * 2 - 1
+    w4 = numpy.random.rand(74, 74) * 2 - 1
+    w5 = numpy.random.rand(74, 74) * 2 - 1
+    w6 = numpy.random.rand(74, 74) * 2 - 1
+    w7 = numpy.random.rand(74, 1) * 2 - 1
     array_to_csv(w1, "w1.csv")
     array_to_csv(w2, "w2.csv")
     array_to_csv(w3, "w3.csv")
     array_to_csv(w4, "w4.csv")
+    array_to_csv(w5, "w5.csv")
+    array_to_csv(w6, "w6.csv")
+    array_to_csv(w7, "w7.csv")
     print("Done.")
