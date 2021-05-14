ad0d7c87ed5a6078461548afee37cfbc64eb5ec7
Virinas-code <alexcode228@gmail.com>
2021-05-14 16:10:09 +0200

    New sizes and biases in random

--- my_engine/nn_random.py
+++ my_engine/nn_random.py
@@ -19,25 +19,27 @@ def array_to_csv(array, csv_path):
     return 0
 
 
-continuer = input("Do you REALLY want to reset w1, w2, w3, w4, w5, w6, w7, w8 and w9 ? [y/N] ")
+continuer = input("Do you REALLY want to reset all weights ? [y/N] ")
 
 if continuer == "y":
-    w1 = numpy.random.rand(813, 1024)
-    w2 = numpy.random.rand(1024, 1024)
-    w3 = numpy.random.rand(1024, 1024)
-    w4 = numpy.random.rand(1024, 1024)
-    w5 = numpy.random.rand(1024, 1024)
-    w6 = numpy.random.rand(1024, 1024)
-    w7 = numpy.random.rand(1024, 1024)
-    w8 = numpy.random.rand(1024, 1024)
-    w9 = numpy.random.rand(1024, 1)
+    w1 = numpy.random.rand(64, 64) * 2 - 1
+    w2 = numpy.random.rand(64, 64) * 2 - 1
+    w3 = numpy.random.rand(64, 64) * 2 - 1
+    w4 = numpy.random.rand(1, 64) * 2 - 1
+    w5 = numpy.random.rand(64, 1) * 2 - 1
+    b1 = numpy.random.rand(64, 64) * 2 - 1
+    b2 = numpy.random.rand(64, 64) * 2 - 1
+    b3 = numpy.random.rand(64, 64) * 2 - 1
+    b4 = numpy.random.rand(1, 64) * 2 - 1
+    b5 = numpy.random.rand(1, 1) * 2 - 1
     array_to_csv(w1, "w1.csv")
     array_to_csv(w2, "w2.csv")
     array_to_csv(w3, "w3.csv")
     array_to_csv(w4, "w4.csv")
     array_to_csv(w5, "w5.csv")
-    array_to_csv(w6, "w6.csv")
-    array_to_csv(w7, "w7.csv")
-    array_to_csv(w8, "w8.csv")
-    array_to_csv(w9, "w9.csv")
+    array_to_csv(b1, "b1.csv")
+    array_to_csv(b2, "b2.csv")
+    array_to_csv(b3, "b3.csv")
+    array_to_csv(b4, "b4.csv")
+    array_to_csv(b5, "b5.csv")
     print("Done.")
