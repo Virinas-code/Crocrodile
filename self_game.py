cf57537fdce85d7064d291d3374c0d9194bdfe26
Virinas-code <alexcode228@gmail.com>
2021-05-23 22:46:55 +0200

    New self game

--- self_game.py
+++ self_game.py
@@ -12,18 +12,12 @@ import my_engine
 crocrodile = my_engine.EngineBase("Crocrodile", "Virinas-code")
 
 
-def calculate(calc_board, clock_time, increment):
+def calculate(calc_board, depth):
     """Run Crocrodile on board."""
-    clock_time = clock_time + increment
-    if clock_time > 1200:
-        best_move = crocrodile.minimax_std(calc_board, 4, board.turn)[1]
-    elif clock_time < 120:
-        best_move = crocrodile.minimax_std(calc_board, 2, board.turn)[1]
-    elif clock_time < 30:
-        best_move = crocrodile.minimax_std(calc_board, 1, board.turn)[1]
-    else:
-        best_move = crocrodile.minimax_std(calc_board, 3, board.turn)[1]
-    return best_move
+    result = crocrodile.minimax_std(calc_board, depth, board.turn)
+    print("* Score : {0}       ".format(result[0]), end="\r")
+    time.sleep(2)
+    return result[1]
 
 
 ITERS = 0
@@ -31,33 +25,17 @@ print("********** Crocrodile self game **********")
 while True:
     ITERS += 1
     print("* Game #{0}".format(ITERS))
-    time_control = float(input("* Time control : ")) * 60
-    time_increment = int(input("* Increment : "))
+    depth = int(input("* Depth : "))
     board = chess.Board()
-    white_clock = time_control
-    black_clock = time_control
-    while not board.outcome() and white_clock > 0 and black_clock > 0:
-        print("* > White : {0}:{1} / Black : {2}:{3}  ".format(int(white_clock // 60),
-                                                           int(white_clock % 60),
-                                                           int(black_clock // 60),
-                                                           int(black_clock % 60)
-                                                           ), end="\r")
-        start = time.time()
-        board.push(calculate(board, white_clock, time_increment))
-        end = time.time()
-        white_clock -= end - start + time_increment
-        print("* White : {0}:{1} / > Black : {2}:{3}  ".format(int(white_clock // 60),
-                                                           int(white_clock % 60),
-                                                           int(black_clock // 60),
-                                                           int(black_clock % 60)
-                                                           ), end="\r")
-        start = time.time()
-        board.push(calculate(board, black_clock, time_increment))
-        end = time.time()
-        black_clock -= end - start + time_increment
+    while not board.outcome():
+        if board.turn == chess.WHITE:
+            print("* > White / Black  ", end="\r")
+        else:
+            print("* White / > Black  ", end="\r")
+        board.push(calculate(board, depth))
     print()
     print("* End FEN :", board.fen())
     if board.outcome().winner is not None:
         print("* Winner : {0}".format("white" if board.outcome().winner else "black"))
     else:
-        print("* Winner : draw")
+        print("* Draw")
