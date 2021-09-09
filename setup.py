e6c5d7f1a743c556c271662c2f38f47f54af9d0a
Virinas-code <alexcode228@gmail.com>
2021-09-09 13:02:35 +0200

    Setup: Silent stderr and other work

--- setup.py
+++ setup.py
@@ -10,10 +10,18 @@ import os
 import stat
 import pkgutil
 import glob
-import requests
+from urllib.request import urlopen, Request
 import zipfile
 import tarfile
 import platform
+import shutil
+
+try:
+    shutil.rmtree("setup-env")
+except FileNotFoundError:
+    pass
+os.mkdir("setup-env")
+os.chdir("setup-env")
 
 
 def load_requirements(requirements):
@@ -28,20 +36,22 @@ def load_requirements(requirements):
 
 
 def download(url, destination):
-    request = requests.get(url, stream=True)
-    request.raw.decode_content = True
+    if not url.startswith("http"):
+        raise RuntimeError("Incorrect and possibly insecure protocol in url")
+    httprequest = Request(url)
+    response = urlopen(httprequest)
     with open(destination, 'wb') as destination:
-        destination.write(request.raw.read())
+        destination.write(response.read())
 
 
 def detect_python():
     """Detect Python installation."""
-    print("Detecting Python installation...", end=" ", flush=True)
-    test = os.system("python3 --version > " + os.devnull)
+    print("Detecting: Python...", end=" ", flush=True)
+    test = os.system("python3 --version > " + os.devnull + " 2> " + os.devnull)
     if test == 0:
         print("Done.")
         return "python3"
-    test = os.system("py -3 --version > " + os.devnull)
+    test = os.system("py -3 --version > " + os.devnull + " 2> " + os.devnull)
     if test == 0:
         print("Done.")
         return "py -3"
@@ -53,8 +63,9 @@ def detect_python():
 
 def detect_pip(python):
     """Detect pip installation."""
-    print("Detecting pip installation...", end=" ", flush=True)
-    test = os.system(python + " -m pip --version > " + os.devnull)
+    print("Detecting: pip...", end=" ", flush=True)
+    test = os.system(python + " -m pip --version > "
+                     + os.devnull + " 2> " + os.devnull)
     print("Done.")
     if test == 0:
         return
@@ -62,7 +73,8 @@ def detect_pip(python):
     download("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
     print("Done.")
     print("Installing: pip...", end=" ", flush=True)
-    test = os.system(python + " get-pip.py > " + os.devnull)
+    test = os.system(python + " get-pip.py > "
+                     + os.devnull + " 2> " + os.devnull)
     print("Done.")
     if test != 0:
         print("/!\\ Failed to install pip.")
@@ -72,9 +84,11 @@ def detect_pip(python):
 def install_requirements(requirements, python):
     """Install missing requirements."""
     for requirement in requirements:
-        print("Downloading: https://pypi.org/simple/{0}/...".format(requirement), end=" ", flush=True)
+        print(
+            "Downloading: https://pypi.org/simple/{0}/...".format(requirement), end=" ", flush=True)
         for retry in range(3):
-            test = os.system(python + " -m pip download --prefer-binary " + requirement + " > " + os.devnull)
+            test = os.system(python + " -m pip download --prefer-binary "
+                             + requirement + " > " + os.devnull + " 2> " + os.devnull)
             if test == 0:
                 break
             if retry == 0:
@@ -86,7 +100,8 @@ def install_requirements(requirements, python):
         print("Done.")
     for wheel in glob.glob("*.whl"):
         print("Installing:", wheel + "...", end=" ", flush=True)
-        test = os.system(python + " -m pip install --no-dependencies " + wheel + " > " + os.devnull)
+        test = os.system(python + " -m pip install --no-dependencies "
+                         + wheel + " > " + os.devnull + " 2> " + os.devnull)
         if test != 0:
             print("\n/!\\ Failed to install", wheel)
         print("Done.")
@@ -99,8 +114,10 @@ def install_requirements(requirements, python):
         print("Installing:", targz[:-7] + "...", end=" ", flush=True)
         os.chdir("setup-" + targz[:-7])
         os.chdir(targz[:-7])
-        os.system(python + " setup.py build > " + os.devnull)
-        os.system(python + " setup.py install > " + os.devnull)
+        os.system(python + " setup.py build > "
+                  + os.devnull + " 2> " + os.devnull)
+        os.system(python + " setup.py install > "
+                  + os.devnull + " 2> " + os.devnull)
         os.chdir("../..")
         print("Done.")
 
@@ -124,12 +141,14 @@ def install_crocrodile(python):
     print("Installing: Crocrodile...", end=" ", flush=True)
     if system == "Linux":
         with open("/usr/bin/crocrodile", "w") as file:
-            file.write("#!/usr/bin/sh\ncd /usr/lib/crocrodile/Crocrodile-master/\n" + python + " uci.py")
+            file.write(
+                "#!/usr/bin/sh\ncd /usr/lib/crocrodile/Crocrodile-master/\n" + python + " uci.py")
             st = os.stat('/usr/bin/crocrodile')
             os.chmod('/usr/bin/crocrodile', st.st_mode | stat.S_IEXEC)
     elif system == "Windows":
         with open("C:/Program Files (x86)/Crocrodile/crocrodile.bat") as file:
-            file.write("cd C:/Program Files (x86)/Crocrodile/Crocrodile-master/\n" + python + " uci.py")
+            file.write(
+                "cd C:/Program Files (x86)/Crocrodile/Crocrodile-master/\n" + python + " uci.py")
     print("Done.")
 
 
@@ -143,12 +162,22 @@ def install(requirements):
 
 def stop():
     """Stop installation."""
+    shutil.rmtree("setup-env")
     print("Installation canceled.")
     sys.exit(1)
 
 
-requirements = ["berserk", "python-chess", "colorama", "pip"]
+def get_requirements():
+    print("Downloading: https://raw.githubusercontent.com/Virinas-code/Crocrodile/master/requirements.txt...", end=" ")
+    download("https://raw.githubusercontent.com/Virinas-code/Crocrodile/master/requirements.txt", "requirements.txt")
+    print("Done.")
+    required = list()
+    for line in open("requirements.txt").readlines():
+        required.append(line.split("==")[0])
+    return required
 
+
+requirements = get_requirements()
 
 print("Welcome in Crocrodile setup program !")
 
@@ -161,7 +190,11 @@ print("Installed packages:")
 print("   ", "\t".join(installed))
 print("Packages to install:")
 print("   ", "\t".join(to_install))
-continuation = input("Continue ? [Y/n] ").lower()
+if not sys.argv:
+    continuation = input("Continue ? [Y/n] ").lower()
+else:
+    continuation = ""
+print()
 if continuation in ("y", "yes", ""):
     install(to_install)
 elif continuation in ("n", "no"):
