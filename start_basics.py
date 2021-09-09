4a3bfc99109bce7d20e444e81992931309f6a846
Virinas-code <alexcode228@gmail.com>
2021-09-09 12:58:59 +0200

    Start: Other start scripts

---
+++ start_basics.py
@@ -0,0 +1,7 @@
+import sys
+import os
+print(os.getcwd())
+import crocrodile
+import crocrodile.nn
+import crocrodile.nn.basics_train
+crocrodile.nn.basics_train.main(sys.argv)
---
+++ start_training.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Crocrodile Training.
+Program for starting training algorithm.
+
+:author: Virinas-code
+"""
+import sys
+import os
+
+print(os.getcwd())
+
+
+def input_algorithm() -> int:
+    """
+    Input training algorithm.
+
+    :return: Training algorithm indice.
+    :rtype: int
+    """
+    print("Select an algorithm :")
+    print("\t1. Masters training")
+    print("\t2. Basics training")
+    while True:
+        algorithm = input("\tAlgorithm :")
+        try:
+            algorithm = int(algorithm)
+        except ValueError:
+            continue
+        if algorithm not in list(range(1, 3)):
+            continue
+        else:
+            return algorithm
+
+
+def main(argv: list) -> int:
+    """
+    Main function.
+
+    :param argv: sys.argv
+    :type argv: list
+    :return: Exit code.
+    :rtype: int
+    """
+    algorithm = input_algorithm()
+    if algorithm == 1:
+        print(os.getcwd())
+        exec(compile(open("my_engine/masters_train.py").read(),
+                     "my_engine/masters_train.py", "exec"))
+    elif algorithm == 2:
+        import my_engine.basics_train
+        my_engine.basics_train.main()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
