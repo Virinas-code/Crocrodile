a972c016a5f0accba4c40f97f2b3dbe9ec53ba7c
Virinas-code <alexcode228@gmail.com>
2021-09-10 13:39:17 +0200

    Client: Lint

--- crocrodile/client/__init__.py
+++ crocrodile/client/__init__.py
@@ -5,28 +5,31 @@ Crocrodile client.
 
 Lichess client for Crocrodile
 """
-import threading
+import copy
 import datetime
-import time
 import sys
+import threading
+import time
+
 import berserk
-import colorama
 import chess
+import colorama
+
 # from Crocrodile.main import yukoo
 import crocrodile
-from crocrodile.nn.load_network import load_network  # Network loader
-import crocrodile.nn
 import crocrodile.engine
-import copy
+import crocrodile.nn
+from crocrodile.nn.load_network import load_network  # Network loader
+
 yukoo = crocrodile.engine.EngineBase("Yukoo", "Virinas-code")
 minimax = yukoo.minimax_std
 
-SPEEDS = ['classical', 'rapid', 'blitz', 'bullet']
-VARIANTS = ['standard', "fromPosition"]
+SPEEDS = ["classical", "rapid", "blitz", "bullet"]
+VARIANTS = ["standard", "fromPosition"]
 
-main_log = open("main.log", 'w')
-error_log = open("error.log", 'w')
-debug_log = open("debug.log", 'w')
+main_log = open("main.log", "w")
+error_log = open("error.log", "w")
+debug_log = open("debug.log", "w")
 
 colorama.init()
 
@@ -39,20 +42,38 @@ def _lok(*args, **kwargs):
         main_log.write(" ".join(str(arg) for arg in args) + "\n")
     else:
         del kwargs["store"]
-    print(colorama.Style.RESET_ALL + colorama.Fore.GREEN
-          + time.asctime(time.localtime()) + ":", *args, **kwargs)
+    print(
+        colorama.Style.RESET_ALL
+        + colorama.Fore.GREEN
+        + time.asctime(time.localtime())
+        + ":",
+        *args,
+        **kwargs
+    )
 
 
 def _ldebug(*args, **kwargs):
     debug_log.write(" ".join(str(arg) for arg in args) + "\n")
-    print(colorama.Style.RESET_ALL + colorama.Fore.MAGENTA
-          + time.asctime(time.localtime()) + ":", *args, **kwargs)
+    print(
+        colorama.Style.RESET_ALL
+        + colorama.Fore.MAGENTA
+        + time.asctime(time.localtime())
+        + ":",
+        *args,
+        **kwargs
+    )
 
 
 def _lerr(*args, **kwargs):
     error_log.write(" ".join(str(arg) for arg in args) + "\n")
-    print(colorama.Style.RESET_ALL + colorama.Fore.RED
-          + time.asctime(time.localtime()) + ":", *args, **kwargs)
+    print(
+        colorama.Style.RESET_ALL
+        + colorama.Fore.RED
+        + time.asctime(time.localtime())
+        + ":",
+        *args,
+        **kwargs
+    )
 
 
 def lnone(*args, **kwargs):
@@ -71,6 +92,13 @@ with open("lichess.token") as file:
 
 session = berserk.TokenSession(token)
 client = berserk.Client(session)
+HELP_USAGE = 'Usage : client.py [-v | -q] [-h] \
+                [-c "user time increment color" | \
+                --challenge "user time increment color" | -a | --auto] \
+                [-n | --neural-network] [-u | --upgrade] [-d | --dev]'
+HELP_CHALLENGE = "\t-c, --challenge \"user time increment color\" : \
+                    Challenge user in time+increment, BOT is playing with \
+                    color ('white' or 'black')"
 
 if len(sys.argv) > 1:
     argc = 0
@@ -84,13 +112,16 @@ if len(sys.argv) > 1:
                 lerr = lnone
             if arg == "-h" or arg == "--help":
                 print(
-                    "Usage : client.py [-v | -q] [-h] [-c \"user time increment color\" | --challenge \"user time increment color\" | -a | --auto] [-n | --neural-network] [-u | --upgrade] [-d | --dev]")
+                    HELP_USAGE
+                )
                 print("Description : Crocrodile Lichess client")
                 print("Commands :")
                 print("\t-h, --help : Show this message and exit")
                 print("\t-v, --verbose : Show debug logs")
                 print("\t-q, --quiet : Don't show any logs")
-                print("\t-c, --challenge \"user time increment color\" : Challenge user in time+increment, BOT is playing with color ('white' or 'black')")
+                print(
+                    HELP_CHALLENGE
+                )
                 print("\t-a, --auto : Auto challenge BOTs")
                 print("\t-n, --neural-network : Enable Neural Network")
                 print("\t-u, --upgrade: Upgrade to bot account")
@@ -143,7 +174,7 @@ class Game(threading.Thread):
         self.initial_fen = fen
         self.client = client
         self.stream = client.bots.stream_game_state(game_id)
-        if color == 'white':
+        if color == "white":
             self.my_turn = chess.BLACK
         else:
             self.my_turn = chess.WHITE
@@ -155,9 +186,9 @@ class Game(threading.Thread):
 
     def run(self):
         for event in self.stream:
-            if event['type'] == 'gameState':
+            if event["type"] == "gameState":
                 self.game_state_change(event)
-            elif event['type'] == 'chatLine':
+            elif event["type"] == "chatLine":
                 self.chat(event)
             elif event["type"] == "gameFull":
                 self.game_full(event)
@@ -166,11 +197,11 @@ class Game(threading.Thread):
 
     def game_state_change(self, event):
         ldebug("game state change", event)
-        if event['status'] == "started":
-            mvs = event['moves'].split(" ")
+        if event["status"] == "started":
+            mvs = event["moves"].split(" ")
             board = chess.Board(self.initial_fen)
             first_pos = True
-            if mvs != ['']:
+            if mvs != [""]:
                 first_pos = False
                 for move in mvs:
                     board.push(chess.Move.from_uci(move))
@@ -178,8 +209,12 @@ class Game(threading.Thread):
             if board.turn == self.my_color:
                 t = event[self.time_control].time()
                 time_s = (t.hour * 60 + t.minute) * 60 + t.second
-                t = event.get("winc", datetime.datetime(
-                    1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc))
+                t = event.get(
+                    "winc",
+                    datetime.datetime(
+                        1970, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc
+                    ),
+                )
                 increment = (t.hour * 60 + t.minute) * 60 + t.second
                 """
                 lok("depth " + str(5) + ")")
@@ -191,35 +226,70 @@ class Game(threading.Thread):
                 else:
                     depth = start_depth(time_s)
                     limit = limit_time(time_s, increment)
-                score = float('-inf')
+                score = float("-inf")
                 best_move = chess.Move.null()
-                lok("Game", self.game_id, "| Maximum "
-                    + str(int(limit)) + "s to calculate")
+                lok(
+                    "Game",
+                    self.game_id,
+                    "| Maximum " + str(int(limit)) + "s to calculate",
+                )
                 limit += time.time()
-                lok("Game", self.game_id, "| Depth "
-                    + str(depth) + ": Calculating...", end="\r")
+                lok(
+                    "Game",
+                    self.game_id,
+                    "| Depth " + str(depth) + ": Calculating...",
+                    end="\r",
+                )
                 last_score, last_best_move = minimax(
-                    board, depth, board.turn, float('inf'))
-                lok("Game", self.game_id, "| Depth " + str(depth) + ": Score "
-                    + str(last_score) + " (best move " + last_best_move.uci() + ")")
+                    board, depth, board.turn, float("inf")
+                )
+                lok(
+                    "Game",
+                    self.game_id,
+                    "| Depth "
+                    + str(depth)
+                    + ": Score "
+                    + str(last_score)
+                    + " (best move "
+                    + last_best_move.uci()
+                    + ")",
+                )
                 while True:
                     depth += 1
-                    lok("Game", self.game_id, "| Depth "
-                        + str(depth) + ": Calculating...", end="\r")
+                    lok(
+                        "Game",
+                        self.game_id,
+                        "| Depth " + str(depth) + ": Calculating...",
+                        end="\r",
+                    )
                     score, best_move = minimax(board, depth, board.turn, limit)
-                    if score == float('inf'):
+                    if score == float("inf"):
                         score = last_score
                         best_move = last_best_move
-                        lok("Game", self.game_id, "| Depth "
-                            + str(depth) + ": Not enough time")
+                        lok(
+                            "Game",
+                            self.game_id,
+                            "| Depth " + str(depth) + ": Not enough time",
+                        )
                         break
                     elif score == 10000 or score == -10000:
                         break
                     else:
-                        lok("Game", self.game_id, "| Depth " + str(depth) + ": Score "
-                            + str(score) + " (best move " + best_move.uci() + ")")
-                        last_score, last_best_move = copy.deepcopy(
-                            score), copy.deepcopy(best_move)
+                        lok(
+                            "Game",
+                            self.game_id,
+                            "| Depth "
+                            + str(depth)
+                            + ": Score "
+                            + str(score)
+                            + " (best move "
+                            + best_move.uci()
+                            + ")",
+                        )
+                        last_score, last_best_move = (
+                            copy.deepcopy(score),
+                            copy.deepcopy(best_move),
+                        )
 
                 retry = 3
                 while retry > 0:
@@ -239,12 +309,19 @@ class Game(threading.Thread):
             else:
                 lok("Game", self.game_id, "| White resigns - Black wins")
         else:
-            lok("Game", self.game_id, "|", event['status'].capitalize())
+            lok("Game", self.game_id, "|", event["status"].capitalize())
             sys.exit(0)
 
     def chat(self, event):
-        lok("Game", self.game_id, "|", event['room'].capitalize(
-        ), "@" + event['username'], "says", event['text'])
+        lok(
+            "Game",
+            self.game_id,
+            "|",
+            event["room"].capitalize(),
+            "@" + event["username"],
+            "says",
+            event["text"],
+        )
 
     def game_full(self, event):
         lok("Game", self.game_id, "| Game full")
@@ -252,8 +329,14 @@ class Game(threading.Thread):
             self.my_color = True
             self.time_control = "wtime"
             lok("Game", self.game_id, "| Playing as White")
-            self.game_state_change({'status': 'started', 'moves': '', 'btime': datetime.datetime(
-                1970, 1, 1, 12), 'wtime': datetime.datetime(1970, 1, 1, 12)})
+            self.game_state_change(
+                {
+                    "status": "started",
+                    "moves": "",
+                    "btime": datetime.datetime(1970, 1, 1, 12),
+                    "wtime": datetime.datetime(1970, 1, 1, 12),
+                }
+            )
         else:
             self.my_color = False
             self.time_control = "btime"
@@ -262,16 +345,25 @@ class Game(threading.Thread):
 
 lok("Token is", token, store=False)
 
-lok("Connected to", client.account.get().get("title", "USER"),
-    client.account.get().get("username", "Anonymous"), store=False)
+lok(
+    "Connected to",
+    client.account.get().get("title", "USER"),
+    client.account.get().get("username", "Anonymous"),
+    store=False,
+)
 lok("Waiting for challenges")
 continue_loop = True
 colors = {}
 fens = {}
 if CHALLENGE:
     print(challenge_time)
-    challenge = client.challenges.create(challenge_user, True, clock_limit=int(float(
-        challenge_time)) * 60, clock_increment=int(challenge_increment), color=challenge_color)
+    challenge = client.challenges.create(
+        challenge_user,
+        True,
+        clock_limit=int(float(challenge_time)) * 60,
+        clock_increment=int(challenge_increment),
+        color=challenge_color,
+    )
     ldebug(challenge)
     if challenge["challenge"]["color"] == "white":
         colors[challenge["challenge"]["id"]] = "black"
@@ -281,48 +373,91 @@ if CHALLENGE:
 while continue_loop:
     for event in client.bots.stream_incoming_events():
         ldebug(event)
-        if event['type'] == 'challenge':
-            if event['challenge']['speed'] in SPEEDS and event['challenge']['variant']['key'] in VARIANTS and not event['challenge']['id'] in colors and event['challenge']['challenger']['id'] != "crocrodile":  # patch-002
-                client.bots.accept_challenge(event['challenge']['id'])
+        if event["type"] == "challenge":
+            if (
+                event["challenge"]["speed"] in SPEEDS
+                and event["challenge"]["variant"]["key"] in VARIANTS
+                and not event["challenge"]["id"] in colors
+                and event["challenge"]["challenger"]["id"] != "crocrodile"
+            ):  # patch-002
+                client.bots.accept_challenge(event["challenge"]["id"])
                 lok("Challenge", event["challenge"]["id"], "| Accepted")
-                colors[event['challenge']['id']] = event['challenge']['color']
+                colors[event["challenge"]["id"]] = event["challenge"]["color"]
                 if event["challenge"]["variant"]["key"] == "fromPosition":
-                    fens[event["challenge"]["id"]
-                         ] = event["challenge"]["initialFen"]
+                    fens[event["challenge"]["id"]] = event["challenge"][
+                        "initialFen"
+                    ]
                 else:
                     fens[event["challenge"]["id"]] = chess.STARTING_FEN
             else:
                 if event["challenge"]["challenger"]["id"] != "crocrodile":
                     if event["challenge"]["color"] == "random":
-                        lok("Challenge", event["challenge"]["id"],
-                            "| Declining because this is a random color challenge")
-                    elif event['challenge']['id'] in colors:
-                        lok("Challenge", event["challenge"]["id"],
-                            "| Declining because this is a rematch")
-                    elif event['challenge']['speed'] not in SPEEDS:
-                        lok("Challenge", event["challenge"]["id"], "| Declining because the bot doesn't play this speed ("
-                            + event["challenge"]["speed"].capitalize() + ")")
-                    elif event['challenge']['variant']['key'] not in VARIANTS:
-                        lok("Challenge", event["challenge"]["id"],
-                            "| Declining because the bot doesn't play this variant (" + event["challenge"]["variant"]["name"] + ")")
+                        lok(
+                            "Challenge",
+                            event["challenge"]["id"],
+                            "| Declining because this is a random color \
+                                challenge",
+                        )
+                    elif event["challenge"]["id"] in colors:
+                        lok(
+                            "Challenge",
+                            event["challenge"]["id"],
+                            "| Declining because this is a rematch",
+                        )
+                    elif event["challenge"]["speed"] not in SPEEDS:
+                        lok(
+                            "Challenge",
+                            event["challenge"]["id"],
+                            "| Declining because the bot doesn't play this \
+                                speed ("
+                            + event["challenge"]["speed"].capitalize()
+                            + ")",
+                        )
+                    elif event["challenge"]["variant"]["key"] not in VARIANTS:
+                        lok(
+                            "Challenge",
+                            event["challenge"]["id"],
+                            "| Declining because the bot doesn't play this \
+                                variant ("
+                            + event["challenge"]["variant"]["name"]
+                            + ")",
+                        )
                     else:
-                        lok("Challenge", event["challenge"]
-                            ["id"], "| Declining")
-                    client.bots.decline_challenge(event['challenge']['id'])
-                    if event['challenge']['id'] in colors:
+                        lok(
+                            "Challenge",
+                            event["challenge"]["id"],
+                            "| Declining",
+                        )
+                    client.bots.decline_challenge(event["challenge"]["id"])
+                    if event["challenge"]["id"] in colors:
                         client.bots.post_message(
-                            event['challenge']['id'], "I don't accept rematches (lot of bugs)")
+                            event["challenge"]["id"],
+                            "I don't accept rematches (lot of bugs)",
+                        )
                 else:
-                    lok("Challenge", event["challenge"]["id"],
-                        "| Challenging @" + event["challenge"]["destUser"]["name"])
-        elif event['type'] == 'gameStart':
+                    lok(
+                        "Challenge",
+                        event["challenge"]["id"],
+                        "| Challenging @"
+                        + event["challenge"]["destUser"]["name"],
+                    )
+        elif event["type"] == "gameStart":
             game = Game(
-                client, event['game']['id'], colors[event['game']['id']], fens[event["game"]["id"]])
+                client,
+                event["game"]["id"],
+                colors[event["game"]["id"]],
+                fens[event["game"]["id"]],
+            )
             game.start()
-        elif event['type'] == 'gameFinish':
+        elif event["type"] == "gameFinish":
             if AUTO_CHALLENGE:
-                challenge = client.challenges.create(challenge_user, True, clock_limit=int(float(
-                    challenge_time)) * 60, clock_increment=int(challenge_increment), color=challenge_color)
+                challenge = client.challenges.create(
+                    challenge_user,
+                    True,
+                    clock_limit=int(float(challenge_time)) * 60,
+                    clock_increment=int(challenge_increment),
+                    color=challenge_color,
+                )
                 if challenge["challenge"]["color"] == "white":
                     colors[challenge["challenge"]["id"]] = "black"
                 else:
@@ -331,8 +466,11 @@ while continue_loop:
             lok("Game", event["game"]["id"], "| Finished")
         elif event["type"] == "challengeDeclined":
             if event["challenge"]["challenger"]["id"] == "crocrodile":
-                lok("Challenge", event["challenge"]["id"],
-                    "| Declined by @" + event["challenge"]["destUser"]["name"])
+                lok(
+                    "Challenge",
+                    event["challenge"]["id"],
+                    "| Declined by @" + event["challenge"]["destUser"]["name"],
+                )
             else:
                 lok("Challenge", event["challenge"]["id"], "| Declined")
         else:
