9060fdf08d915f5bf8eb5a9520033aedbb31520e
Virinas-code <alexcode228@gmail.com>
2021-09-27 09:27:59 +0200

    Scripts: Reverse training files

---
+++ scripts/reverse_training_file.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+reverse_training_file
+=====================
+Reverse positions and good moves in file.
+"""
+import sys  # Using sys.argv
+
+
+def reverse_training_file(path, dest):
+    """Reverse training file at path.
+
+    :param str path: Path to training file.
+    :param str dest: Destination file.
+    :return: None
+    """
+    print(f"Reading {path}  ...", end=" ")
+    file = open(path).read()
+    print(f"Done.\nParsing {path}  ...", end=" ")
+    content = file.split("\n\n")
+    print(f"Done.\nReversing {path}...", end=" ")
+    content.reverse()
+    print(f"Done.\nJoining {path}  ...", end=" ")
+    content = "\n\n".join(content)
+    print(f"Done.\nWriting {dest}  ...", end=" ")
+    dest = open(dest, "w")
+    dest.write(content)
+    print("Done.")
+
+
+if __name__ == '__main__':
+    input_path = "training_files/" + input("1. Input file  : ")  # Path to training file
+    output_path = "training_files/" + input("2. Output file : ")  # Destination file
+    reverse_training_file(input_path, output_path)
