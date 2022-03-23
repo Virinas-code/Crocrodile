6a212c283e37b2ef5cd07e85a2a499d7ca7c21c3
Virinas-code <alexcode228@gmail.com>
2022-03-23 19:08:24 +0100

    : = . Philosophie Windaubiene

--- crocrodile/nn/basics_train.py
+++ crocrodile/nn/basics_train.py
@@ -851,7 +851,7 @@ class BasicsTrain:
     def main(self, argv):
         """Start training."""
         self.ask()
-        performance_output_file = "nns/log/" + str(datetime.datetime.now()) + ".log"
+        performance_output_file = ("nns/log/" + str(datetime.datetime.now()) + ".log").replace(":", ".")
         with open(performance_output_file, "w") as file: pass
         good_moves_file = self.config["good_moves"]
         good_moves_list = self.parse_good_moves(good_moves_file)
